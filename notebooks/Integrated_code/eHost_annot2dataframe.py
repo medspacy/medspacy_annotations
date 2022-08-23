0fda42add9e0c079b8592e94bdb94e9114b6e6e5
Mengke Hu <mengke.hu@gmail.com>
2022-08-23 14:35:27 -0600

    update Integrated_code/eHost_annot2dataframe.py to fit latest medspacy-io; Adjust pipeline.ipynb accordingly

--- notebooks/Integrated_code/eHost_annot2dataframe.py
+++ notebooks/Integrated_code/eHost_annot2dataframe.py
@@ -6,30 +6,33 @@ from medspacy_io.reader import EhostDirReader
 from spacy.lang.en import English
 from spacy.tokens import Doc
 import pandas as pd
+from typing import List
 
-def eHost_dir_reader(proj_schema:str,corpus_dir:str) -> list[Doc]: #non-overlapped entities
+
+def eHost_dir_reader(proj_schema:str,corpus_dir:str) -> List[Doc]: #non-overlapped entities
     dir_reader = EhostDirReader(nlp=English(), recursive=True, schema_file=proj_schema)
     docs = dir_reader.read(txt_dir=corpus_dir)
     return docs
 
-def eHost_dir_reader_overlap(proj_schema:str,corpus_dir:str) -> list[Doc]: #overlapped entities #only for new version of medspacy-io
+def eHost_dir_reader_overlap(proj_schema:str,corpus_dir:str) -> List[Doc]: #overlapped entities #only for new version of medspacy-io
     dir_reader = EhostDirReader(nlp=English(), support_overlap=True,recursive=True, schema_file=proj_schema)
     docs = dir_reader.read(txt_dir=corpus_dir)
     return docs
 
-def df_builder_overlapSpan(docs:list[Doc],files:list[str])->pd.DataFrame:
+def df_builder_overlapSpan(docs:List[Doc])->pd.DataFrame: #modified for the latest medspacy-io
     data = []
-    for i in range(len(docs)):
-        doc_concepts_dic = docs[i]._.concepts
-        for concept, listSpan in doc_concepts_dic.items():
-            for sp in listSpan:
+    for doc in docs:
+        doc_concepts_list = doc._.concepts #now it is a list to used as key to extract SpanGroup
+        for concept in doc_concepts_list:
+            for sp in doc.spans[concept]: # iterate spans in certain spanGroup
                 dicElem = {}
                 dicElem['Span Text'] = sp.text
                 dicElem['Concept Label'] = sp.label_
                 dicElem['start loc'] = sp.start_char
                 dicElem['end loc'] = sp.end_char
-                dicElem['doc name'] = files[i]
+                dicElem['doc name'] = doc._.doc_name
                 data.append(dicElem)
     df = pd.DataFrame(data)
     return df
     
+ 
